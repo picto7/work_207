174b133efa35bd8fd2cb7c43d75d898b8cdc7b13
picto7 <picto7@gmail.com>
2017-12-17 16:12:18 +0300

    задачи по олимпиаде

--- olympiada/task_4_2017-18.py
+++ olympiada/task_4_2017-18.py
@@ -1,4 +1,18 @@
 # задача 4 Лифт в бизнес-центре ВСОШ (2017-18)
 # муниципальный этап
 
-
+K = int(input())
+N = int(input())
+time = 0
+ost = 0
+for i in range(1, N + 1):
+    a = int(input())
+    if a > 0:
+        a += ost
+        if a // K > 0:
+            time += i * 2 * (a // K)
+        ost = a % K
+        index = i
+if ost > 0:
+    time += index * 2
+print(time)
